1c56d2d44a2949fcddee83744ed4694b10ed4e1e
kiddos <josephyu831007@yahoo.com.tw>
2016-05-07 23:01:42 +0800

    color constants

--- src/window.h
+++ src/window.h
@@ -2,16 +2,32 @@
 #define WINDOW_H
 
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 #include <math.h>
+#include <string.h>
 
 #include "game.h"
 
 namespace reversi {
 
+struct Rect {
+  double x, y, w, h;
+  Rect(const double x, const double y, const double w, const double h)
+      : x(x), y(y), w(w), h(h) {}
+};
+
+struct Color {
+  int r, g, b;
+  Color(int r, int g, int b) : r(r), g(g), b(b) {}
+  Color(const Color& c) : r(c.r), g(c.g), b(c.b) {}
+  Color& operator= (const Color& c) {r=c.r;g=c.g;b=c.b; return *this;}
+};
+
 class MCoord {
  public:
-  MCoord(const int x, const int y, const int size);
+  explicit MCoord(const Rect r);
   MCoord(const MCoord& mcoord);
   MCoord& operator= (const MCoord& mcoord);
 
@@ -20,9 +36,29 @@ class MCoord {
   Move getmove() const;
 
  private:
-  const int X, Y, SIZE;
+  const Rect r;
   const double BOX;
   int mx, my;
+};
+
+class ScorePanel {
+ public:
+  ScorePanel();
+  ScorePanel(const Rect r, const int fontsize);
+  ScorePanel(const ScorePanel& panel);
+  ScorePanel& operator= (const ScorePanel& panel);
+  ~ScorePanel();
+
+  void update(const int wcount, const int bcount);
+  void render() const;
+
+ private:
+  Rect r;
+  int FONT_SIZE;
+
+  ALLEGRO_FONT* font;
+  char whitecount[64];
+  char blackcount[64];
 };
 
 class Window {
@@ -30,24 +66,26 @@ class Window {
   Window();
   ~Window();
   void render();
+  void update();
   bool mainloop();
+
+  static const Color BG, BOARD_BG, BOARD_BORDER;
+  static const Color WHITE_DISC, BLACK_DISC;
 
  private:
   void init();
   const int WIDTH, HEIGHT, PADDING;
   const double FPS;
+
   ALLEGRO_DISPLAY *display;
   ALLEGRO_EVENT_QUEUE *equeue;
   ALLEGRO_TIMER *timer;
   ALLEGRO_EVENT event;
   bool redraw;
 
+  ScorePanel scorepanel;
   Game game;
   int dboard[8][8];
-
-  ALLEGRO_COLOR bg;
-  ALLEGRO_COLOR boardbg, boardline;
-  ALLEGRO_COLOR whitedisc, blackdisc;
 };
 
 }
