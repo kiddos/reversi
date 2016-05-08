832d13037ce641be830d4e232c69f63dde117818
kiddos <josephyu831007@yahoo.com.tw>
2016-05-08 13:29:32 +0800

    static function for processing input

--- src/ai.cpp
+++ src/ai.cpp
@@ -10,8 +10,19 @@ using std::vector;
 
 namespace reversi {
 
+const int AI::INPUT_NODES = 66;
+const int AI::HIDDEN_NODES = 360;
+const int AI::OUTPUT_NODES = 1;
+
 AI::AI() {
   srand(time(NULL));
+}
+
+AI::AI(const AI& ai) : current(ai.current) {}
+
+AI& AI::operator= (const AI& ai) {
+  current = ai.current;
+  return *this;
 }
 
 Move AI::decidemove(const Board current) {
@@ -30,6 +41,27 @@ Move AI::decidemove(const Board current) {
 #endif
 
   return move;
+}
+
+void AI::toinput(const Board b, const Move m, const int turn, int data[66]) {
+  // first 64 nodes are board state
+  int board[BOARDSIZE][BOARDSIZE] = {{0}};
+  b.copyboard(board);
+  for (int i = 0 ; i < BOARDSIZE ; ++i) {
+    for (int j = 0 ; j < BOARDSIZE ; ++j) {
+      if (board[i][j] == turn) {
+        data[i*BOARDSIZE+j] = 1;
+      } else if (board[i][j] == Board::EMPTY) {
+        data[i*BOARDSIZE+j] = 0;
+      } else {
+        data[i*BOARDSIZE+j] = -1;
+      }
+    }
+  }
+
+  // last 2 nodes is next move
+  data[INPUT_NODES-3] = m.x;
+  data[INPUT_NODES-2] = m.y;
 }
 
 }
--- src/ai.h
+++ src/ai.h
@@ -5,13 +5,21 @@
 #include <ctime>
 
 #include "board.h"
+#include "nnet/nnet.h"
 
 namespace reversi {
 
 class AI {
  public:
   AI();
+  AI(const AI& ai);
+  AI& operator= (const AI& ai);
   Move decidemove(const Board current);
+
+  static const int INPUT_NODES;
+  static const int HIDDEN_NODES;
+  static const int OUTPUT_NODES;
+  static void toinput(const Board b, const Move m, const int turn, int data[66]);
 
  private:
   Board current;
