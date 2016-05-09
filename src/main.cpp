0605c482156307c729ede838e2986cb59d5d9980
kiddos <josephyu831007@yahoo.com.tw>
2016-05-10 02:37:35 +0800

    main program training

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "window.h"
+#include "aitrainer.h"
 
 using reversi::Window;
+using reversi::AITrainer;
 
-int main(void) {
-  Window window;
-  window.mainloop();
-
+int main(int argc, char *argv[]) {
+  if (argc == 3) {
+    const int instances = atoi(argv[1]);
+    const int iterations = atoi(argv[2]);
+    AITrainer trainer;
+    trainer.train(instances, iterations);
+    trainer.saveparam();
+  } else {
+    Window window;
+    window.mainloop();
+  }
   return 0;
 }
